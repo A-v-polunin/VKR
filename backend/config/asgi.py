48bae57ee2992cf8216b0cb8fb5d8982187fa9e2
Александр <aleksandr@MacBook-Pro-Aleksandr.local>
2026-06-04 00:34:13 +0300

    fixasgi

--- backend/config/asgi.py
+++ backend/config/asgi.py
@@ -1,14 +1,18 @@
 import os
+import django
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
+django.setup()
 
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
 from config.routing import websocket_urlpatterns
 
+django_asgi_app = get_asgi_application()
+
 application = ProtocolTypeRouter({
-    "http": get_asgi_application(),
+    "http": django_asgi_app,
     "websocket": AuthMiddlewareStack(
         URLRouter(
             websocket_urlpatterns
